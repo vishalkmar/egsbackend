25ccc5a1f64c956f8b6f59962f00f5f0a141d5c1
vishalkmar <vk722413@gmail.com>
2026-02-19 16:46:10 +0530

    change a bit to test

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on PORT ${PORT}`);
   });
 });
